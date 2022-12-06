f561f53d509c9ec892fcc5889e395fd23f7fdcb9
Stephen K <stephenarchimedes@gmail.com>
2022-12-06 09:52:36 +0300

    add linting

--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -1,20 +1,20 @@
-import express from 'express';
-import path, { resolve } from 'path';
+import express, { Request, Response } from 'express';
+import path from 'path';
 import sharp from 'sharp';
 import * as fs from 'fs';
 const resize = express.Router();
 
-//BASE file directory for the assets
+// BASE file directory for the assets
 const BASE_URL: string = path.join(__dirname, '../../../', 'assets');
 
 const errorMsg: string =
   'The following error occured processing your image. Remedy and try again: Error:';
 
-resize.get('/', (req, res) => {
-  let filename: string = <string>req.query.filename ?? null;
-  let width: string = <string>req.query.width ?? null;
-  let height: string = <string>req.query.height ?? null;
-  if (filename) {
+resize.get('/', (req: Request, res: Response) => {
+  const filename: string = (req.query.filename ?? null) as string;
+  const width: string = (req.query.width ?? null) as string;
+  const height: string = (req.query.height ?? null) as string;
+  if (filename != null) {
     if (
       fs.existsSync(
         path.join(BASE_URL, 'thumb', `${filename}_thumb_${width}_${height}.jpg`)
@@ -26,6 +26,7 @@ resize.get('/', (req, res) => {
     } else {
       generateThumb(filename, parseInt(width), parseInt(height))
         .catch((error) => {
+          console.error(error);
           res.send('Error: Input file is missing');
         })
         .finally(() => {
@@ -48,12 +49,12 @@ const generateThumb = async (
   width: number,
   height: number
 ): Promise<unknown> => {
-  return new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
     resolve(
       sharp(path.join(BASE_URL, 'full', `${filename}.jpg`))
         .resize(width, height, {
           // fit: sharp.fit.inside,
-          withoutEnlargement: true,
+          withoutEnlargement: true
         })
         .toFormat('jpeg')
         .toFile(
